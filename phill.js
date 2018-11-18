029dd173ac50d5c7f81d2f84ac9d710a280a06c6
phillipstallworthy <phillipstallworthy@yahoo.com>
2018-11-18 15:18:29 +1300

    working

--- phill.js
+++ phill.js
@@ -16,39 +16,43 @@ window.onload = function () {
       var side = sideCalc();
       var depth = up((side / 2) + margin, margin, side);
 
-      down((side / 2) + margin,depth + margin,
-          (side / 2));
+      down((side / 2) + margin, depth + margin,
+        (side / 2));
 
     }
 
+    //colours = []
+
     function fiveTriangles() {
       var side = sideCalc();
-      var depth = up((side / 2) + margin, margin, side);
+      var depth = up((side / 2) + margin, margin, side, "blue");
 
-      var depth1 = down((side / 2) + margin,     depth + margin, (side / 2));
+      var depth1 = down((side / 2) + margin, depth + margin,(side / 2), "yellow");
+ 
+      var depth2 = three();
 
-      // create triagle to the left, right, and above, the one 
-      // just created in the line above
-      down((side / 4) + margin,     depth + margin, (side / 4));
-      //create 3 here as well - recursive.... :)
-      down((side / 4) * 3 + margin, depth + margin, (side / 4));
-      down((side / 2) + margin,     depth1 + margin, (side / 4));
+      down((side / 8) + margin, depth + margin, (side / 8), "green");
+      down((side / 8) * 3 + margin, depth + margin, (side / 8), "green");
+      down((side / 4) + margin, (depth - depth2) + margin, (side / 8), "green");
 
-      down((side / 8) + margin,     depth + margin, (side / 8))
+      down((side / 16) + margin, depth + margin, (side / 16), "purple");
 
-      down((side / 16) + margin,     depth + margin, (side / 16))
+      down((side / 32) + margin, depth + margin, (side / 32), "orange");
 
-      down((side / 32) + margin,     depth + margin, (side / 32))
+      down((side / 64) + margin, depth + margin, (side / 64), "white");
 
-      down((side / 64) + margin,     depth + margin, (side / 64))
 
-      down((side / 64) + margin,     depth + margin, (side / 64))
-
+      function three(size, colour) {
+        down((side / 4) + margin, depth + margin, (side / 4), "red");
+        down((side / 4) * 3 + margin, depth + margin, (side / 4), "red");
+        var depth2 = down((side / 2) + margin, depth1 + margin, (side / 4), "red");
+        return depth2;
+      }
     }
 
     // draw an upside down equilateral triangle
     // path starts at x,y, bottom  
-    function down(x, y, side) {
+    function down(x, y, side, colour) {
       var half = side / 2;
       var angle = (Math.PI / 180) * 60;
       var depth = Math.cos(angle / 2) * side;
@@ -68,15 +72,15 @@ window.onload = function () {
 
       chaos.context.closePath();
       chaos.context.stroke();
-      chaos.context.fillStyle = "red";
-      chaos.context.fill(); 
+      chaos.context.fillStyle = colour;
+      chaos.context.fill();
 
       return depth;
     }
 
     // draw an equilateral triangle
     // path starts at x,y, top middle 
-    function up(x, y, side) {
+    function up(x, y, side, colour) {
       var half = side / 2;
       var angle = (Math.PI / 180) * 60;
       var depth = Math.cos(angle / 2) * side;
@@ -97,8 +101,8 @@ window.onload = function () {
       //chaos.context.lineTo(x, y);
       chaos.context.closePath();
       chaos.context.stroke();
-      chaos.context.fillStyle = "blue";
-      chaos.context.fill();      
+      chaos.context.fillStyle = colour;
+      chaos.context.fill();
 
       return depth;
     }
